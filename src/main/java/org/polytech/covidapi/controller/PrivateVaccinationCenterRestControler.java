04f94651f4177f5ad29700b33fbde3246c345891
Florian <florian.f.sirryus@gmail.com>
2022-11-28 08:31:42 +0100

    adding

---
+++ src/main/java/org/polytech/covidapi/controller/PrivateVaccinationCenterRestControler.java
@@ -0,0 +1,25 @@
+package org.polytech.covidapi.controller;
+
+import java.util.List;
+
+import org.polytech.covidapi.entity.VaccinationCenter;
+import org.polytech.covidapi.repository.VaccinationCenterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrivateVaccinationCenterRestControler 
+{  
+    @Autowired
+    private VaccinationCenterRepository centerRepository;
+
+    @GetMapping(path="api/private/center")
+     public List<VaccinationCenter> getVaccinatedCenter(@RequestParam("city") String city)
+     {
+         //return centerRepository.findAllByCityLike("%"+city+"%");
+         return List.of(new VaccinationCenter(1,"Bar Polytech", "2 Rue Jean Lamour", "54500", "Nancy",List.of()));
+     }
+ }
+
