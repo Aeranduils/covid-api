b5614c24870b91da39a553aee9e828eb0e8967db
Florian.F <38659111+Aeranduils@users.noreply.github.com>
2023-01-07 17:04:33 +0100

    bin

---
+++ bin/main/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO utilisateur VALUES ('1', 'user','$2y$10$6nAqZLorMlfK0B0kSayWSOUuVbmuPyF.zmS96BsF.qXFC24BiY3hO')
+
+
+INSERT INTO VACCINATION_CENTER
+VALUES (1, '30 rue Prouvé', 'Nancy', 'Centre 1', '54000');
+INSERT INTO VACCINATION_CENTER 
+VALUES (2, '2 rue Poincaré', 'Nancy', 'Centre 2', '54000');
+INSERT INTO VACCINATION_CENTER
+VALUES (3, '7 Avenue Leclerc', 'Vandoeuvre', 'Centre 3', '54500');
+
+
+INSERT INTO doctor VALUES (1, 'Henry');
