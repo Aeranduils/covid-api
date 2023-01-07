ae14efe5b3148046cd8bcb400b37da491cb6f8cf
Guyak <guyotantonin2001@gmail.com>
2023-01-07 15:37:41 +0100

    modif build

--- Fullstack_Jenkins/covid-api/src/main/resources/import.sql
+++ Fullstack_Jenkins/covid-api/src/main/resources/import.sql
@@ -14,58 +14,11 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `appointment`
---
-
-CREATE TABLE `appointment` (
-  `id_appointment` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `id_center` int(11) NOT NULL,
-  `id_patient` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `hibernate_sequence`
---
-
-CREATE TABLE `hibernate_sequence` (
-  `next_val` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
 -- Contenu de la table `hibernate_sequence`
 --
 
 INSERT INTO `hibernate_sequence` (`next_val`) VALUES
 (1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `patient`
---
-
-CREATE TABLE `patient` (
-  `id_patient` int(11) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `mail` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `utilisateur`
---
-
-CREATE TABLE `utilisateur` (
-  `id` bigint(20) NOT NULL,
-  `login` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `utilisateur`
@@ -73,20 +26,6 @@ CREATE TABLE `utilisateur` (
 
 INSERT INTO `utilisateur` (`id`, `login`, `password`) VALUES
 (1, 'user', '$2a$12$ZPeXqNjHJpSq2Roh3M6ViOxd9kBW1kqKKVMfzlQKJqePZ9SpABpYy');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `vaccination_center`
---
-
-CREATE TABLE `vaccination_center` (
-  `id_center` int(11) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `city` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `postal_code` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `vaccination_center`
@@ -97,48 +36,3 @@ INSERT INTO `vaccination_center` (`id_center`, `address`, `city`, `name`, `posta
 (2, 'Place Carnot', 'Nancy', 'NancyV2', '54000'),
 (3, 'Vélodrome', 'Vandoeuvre', 'Autre', '54500'),
 (4, 'Gare de Metz', 'Metz', 'Centre de la Gare', '57000');
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `appointment`
---
-ALTER TABLE `appointment`
-  ADD PRIMARY KEY (`id_appointment`),
-  ADD KEY `fk_idCenter` (`id_center`),
-  ADD KEY `fk_idPatient` (`id_patient`);
-
---
--- Index pour la table `patient`
---
-ALTER TABLE `patient`
-  ADD PRIMARY KEY (`id_patient`);
-
---
--- Index pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `vaccination_center`
---
-ALTER TABLE `vaccination_center`
-  ADD PRIMARY KEY (`id_center`);
-
---
--- Contraintes pour les tables exportées
---
-
---
--- Contraintes pour la table `appointment`
---
-ALTER TABLE `appointment`
-  ADD CONSTRAINT `fk_idCenter` FOREIGN KEY (`id_center`) REFERENCES `vaccination_center` (`id_center`),
-  ADD CONSTRAINT `fk_idPatient` FOREIGN KEY (`id_patient`) REFERENCES `patient` (`id_patient`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
